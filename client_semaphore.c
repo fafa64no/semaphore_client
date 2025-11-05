6bf27685221903a22709419076d5d4d7074d2c35
Sébastien Nouira Huet <148809822+fafa64no@users.noreply.github.com>
2025-11-05 08:28:49 +0100

    [UPDATE] Switched the project to C

---
+++ client_semaphore.c
@@ -0,0 +1,22 @@
+//
+// Created by sebas on 11/5/25.
+//
+
+#include "client_semaphore.h"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "segdef.h"
+
+int main() {
+    printf("Starting program");
+    init_rand();
+
+
+
+    return EXIT_SUCCESS;
+}
